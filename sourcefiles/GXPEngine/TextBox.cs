60acceebfd2c76000292ba6b6f366ef4881026a2
Fishey <randybrussen@gmail.com>
2014-11-21 12:52:22 +0100

    Textbox base
    Doesn't do shit yet

---
+++ sourcefiles/GXPEngine/TextBox.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+
+namespace GXPEngine
+{
+	public class TextBox : Canvas
+	{
+		Font font;
+		Brush brush;
+		PointF position;
+		public TextBox () : base(1280,64)
+		{
+			font = new Font ("Arial", 20, FontStyle.Regular);
+			brush = new SolidBrush (Color.Black);
+			position = new PointF (0, 0);
+		}
+
+		public void DrawTextBox(string name, string text)
+		{
+			graphics.Clear (Color.Black);
+			DrawText (name, text);
+
+		}
+		private void DrawText(string name, string text)
+		{
+			string message = "The {0} says: " + text + ".";
+			position = new PointF (0, 0);
+			brush = new SolidBrush (Color.Black);
+			graphics.DrawString (message, font, brush, position);
+		}
+	}
+}
+
